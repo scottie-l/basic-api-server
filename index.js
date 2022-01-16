b68baecfbd7ab2ff7b2dac76a6cdef9921fd961d
Scott Lease <scottie_lease@hotmail.com>
2022-01-15 16:18:53 -0800

    added psuedo code and some routing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
+require('dotenv').config();
 const { start } = require('./lib/server.js');
 const { db } = require('./lib/models');
 const PORT = process.env.PORT || 3000;
 
 // in order to perform operations against our db, we have to intialize
-
 // create tables and initizes them before we start our server.
+
 db.sync()
   .then(() => start(PORT))
   .catch(err => console.log(err));
